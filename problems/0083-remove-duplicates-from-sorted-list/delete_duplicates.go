0ea4c971e31eb9007635384d51484d07eb7de28c
evleria <valeryia.vidzevich@gmail.com>
2020-11-09 22:29:35 +0300

    delete duplicates

---
+++ problems/0083-remove-duplicates-from-sorted-list/delete_duplicates.go
@@ -0,0 +1,22 @@
+package _083_remove_duplicates_from_sorted_list
+
+import (
+	. "github.com/evleria/leetcode-in-go/structs"
+)
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	current := head
+
+	for current.Next != nil {
+		if current.Val == current.Next.Val {
+			current.Next = current.Next.Next
+		} else {
+			current = current.Next
+		}
+	}
+
+	return head
+}
---
+++ problems/0083-remove-duplicates-from-sorted-list/delete_duplicates_test.go
@@ -0,0 +1,31 @@
+package _083_remove_duplicates_from_sorted_list
+
+import (
+	. "github.com/evleria/leetcode-in-go/structs"
+	"testing"
+
+	"gotest.tools/v3/assert"
+	is "gotest.tools/v3/assert/cmp"
+)
+
+func TestDeleteDuplicates(t *testing.T) {
+	testCases := []struct {
+		got  []int
+		want []int
+	}{
+		{
+			got:  []int{1, 2, 3},
+			want: []int{1, 2, 3},
+		},
+		{
+			got:  []int{1, 1, 2},
+			want: []int{1, 2},
+		},
+	}
+
+	for _, testCase := range testCases {
+		actual := deleteDuplicates(FromSlice(testCase.got)).ToSlice()
+
+		assert.Check(t, is.DeepEqual(actual, testCase.want), testCase.got)
+	}
+}
