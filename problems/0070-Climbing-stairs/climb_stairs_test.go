57af87149a91078e28253a58bc2780638edbff1f
evleria <valeryia.vidzevich@gmail.com>
2020-11-12 23:08:20 +0300

    Climbing Stairs

---
+++ problems/0070-Climbing-stairs/climb_stairs_test.go
@@ -0,0 +1,38 @@
+package _070_Climbing_stairs
+
+import (
+	"testing"
+
+	"gotest.tools/v3/assert"
+	is "gotest.tools/v3/assert/cmp"
+)
+
+func TestClimbStairs(t *testing.T) {
+	testCases := []struct {
+		got  int
+		want int
+	}{
+		{
+			got:  2,
+			want: 2,
+		},
+		{
+			got:  3,
+			want: 3,
+		},
+		{
+			got:  4,
+			want: 5,
+		},
+		{
+			got:  5,
+			want: 8,
+		},
+	}
+
+	for _, testCase := range testCases {
+		actual := climbStairs(testCase.got)
+
+		assert.Check(t, is.Equal(actual, testCase.want), testCase.got)
+	}
+}
