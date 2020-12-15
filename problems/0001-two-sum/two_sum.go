b508ceac3ceb3ed21d07eb087c7e4f97982e2891
evleria <valeryia.vidzevich@gmail.com>
2020-12-15 23:56:02 +0300

    Code style

--- problems/0001-two-sum/two_sum.go
+++ problems/0001-two-sum/two_sum.go
@@ -1,7 +1,6 @@
 package _001_two_sum
 
 func twoSum(nums []int, target int) []int {
-
 	dict := make(map[int]int, len(nums))
 	for i, n := range nums {
 		if j, ok := dict[target-n]; ok {
--- problems/0066-plus-one/plus_one.go
+++ problems/0066-plus-one/plus_one.go
@@ -1,7 +1,6 @@
 package _066_plus_one
 
 func plusOne(digits []int) []int {
-
 	for i := len(digits) - 1; i >= 0; i-- {
 		if digits[i] == 9 {
 			digits[i] = 0
