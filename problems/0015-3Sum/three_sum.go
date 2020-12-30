ff69ef99df64056749f9e56a83bbd90b16fc398a
evleria <valeryia.vidzevich@gmail.com>
2020-12-31 00:39:31 +0300

    15. 3Sum (optimization)

--- problems/0015-3Sum/three_sum.go
+++ problems/0015-3Sum/three_sum.go
@@ -3,25 +3,19 @@ package _015_3Sum
 import "sort"
 
 func threeSum(nums []int) [][]int {
-	if len(nums) < 3 {
-		return [][]int{}
-	}
-
-	result := make([][]int, 0, 4)
+	result := [][]int{}
 	sort.Ints(nums)
 	for i := 0; i < len(nums)-2; i++ {
 		left, right := i+1, len(nums)-1
 		for left < right {
 			sum := nums[i] + nums[left] + nums[right]
-
 			switch {
 			case sum == 0:
 				result = append(result, []int{nums[i], nums[left], nums[right]})
-				for ; left < len(nums)-1 && nums[left] == nums[left+1]; left++ {
+				for ; left < right && nums[left] == nums[left+1]; left++ {
 				}
-				for ; right > 0 && nums[right] == nums[right-1]; right-- {
+				for ; right > left && nums[right] == nums[right-1]; right-- {
 				}
-
 				left++
 				right--
 			case sum > 0:
@@ -30,7 +24,6 @@ func threeSum(nums []int) [][]int {
 				left++
 			}
 		}
-
 		for ; i < len(nums)-1 && nums[i] == nums[i+1]; i++ {
 		}
 	}
