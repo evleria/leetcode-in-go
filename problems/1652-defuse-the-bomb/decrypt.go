d1cdcc71389957689721e4afd7bab16de6a1d8c3
evleria <valeryia.vidzevich@gmail.com>
2021-05-29 00:17:12 +0300

    1652. Defuse the Bomb

--- problems/1652-defuse-the-bomb/decrypt.go
+++ problems/1652-defuse-the-bomb/decrypt.go
@@ -1,25 +1,25 @@
 package _652_defuse_the_bomb
 
-// 12345 | k = 3 | 9(234)  12(345) 10(451) 8(512) 6(123)
-// 345   | k = -3| 12(345) 9(234)  6(123)  8(512) 10(451)
-// -5 +2
-
 func decrypt(code []int, k int) []int {
-	result := make([]int, len(code))
+	n := len(code)
+	result := make([]int, n)
 	if k == 0 {
 		return result
 	}
-	if k > 0 {
-		window := 0
-		for i := 1; i <= k; i++ {
-			window += code[i]
-		}
-		result[0] = window
 
-		for i := 1; i < len(code); i++ {
-			window += code[(k+i)%len(code)] - code[i]
-			result[i] = window
-		}
+	l, r := 1, k
+	if k < 0 {
+		l, r = n+k, n-1
+	}
+
+	window := 0
+	for i := l; i <= r; i++ {
+		window += code[i]
+	}
+
+	for offset := 0; offset < len(code); offset++ {
+		result[offset] = window
+		window += code[(r+offset+1)%n] - code[(l+offset)%n]
 	}
 	return result
 }
