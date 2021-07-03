9ccbe8bbfdc70ff824d6d086b1e539adfd65dcb2
evleria <valeryia.vidzevich@gmail.com>
2021-07-03 18:17:27 +0300

    Fixed test 0049

--- problems/0049-group-anagrams/group_anagrams_test.go
+++ problems/0049-group-anagrams/group_anagrams_test.go
@@ -1,6 +1,7 @@
 package _049_group_anagrams
 
 import (
+	"sort"
 	"testing"
 
 	"gotest.tools/v3/assert"
@@ -37,6 +38,28 @@ func TestGroupAnagrams(t *testing.T) {
 	for _, testCase := range testCases {
 		actual := groupAnagrams(testCase.got)
 
-		assert.Check(t, is.DeepEqual(actual, testCase.want))
+		assert.Check(t, is.DeepEqual(determine(actual), determine(testCase.want)))
 	}
+}
+
+func determine(data [][]string) [][]string {
+	sort.Slice(data, func(i, j int) bool {
+		li, lj := len(data[i]), len(data[j])
+		if li != lj {
+			return li < lj
+		}
+
+		sort.Strings(data[i])
+		sort.Strings(data[j])
+
+		for k := 0; k < li; k++ {
+			if data[i][k] != data[j][k] {
+				return data[i][k] < data[j][k]
+			}
+		}
+
+		return false
+	})
+
+	return data
 }
